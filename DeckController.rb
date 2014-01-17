636c23a397d0773a1134af24d47ef9384bb40c10
dbc-apprentice <apprentice@devbootcamp.com>
2014-01-17 14:24:54 -0600

    Controller and View Take #1

--- DeckController.rb
+++ DeckController.rb
@@ -1,7 +1,34 @@
+## READ, WHAT DONT KNOW, GOOGLE TO FIND OUT THESE PIECES, PSUEDOCODE
+
+## As a user, I want to be able to guess a word.
+## As a user, I want to see if my guess was correct or not.
+## As a user, I want to exit the program if I am SICK OF IT!!!!!!!!!
+
+require_relative 'DeckView' 
+
 class DeckController
 
   def initialize
     @deck = Deck.new
   end
 
+  def run
+    input = ""
+    IntroductoryView.new.render
+    until input == "end"
+      card = @deck.get_next_card
+      DefinitionView.new.render(card.definition)
+      outcome = false
+      until outcome  
+        input = TakeInputView.new.render
+        break if input == "end"
+        outcome = card.match?(input)
+        FeedbackView.new.render(outcome)
+      end
+    end
+  end
+
 end
+
+
+DeckController.new.run
---
+++ DeckView.rb
@@ -0,0 +1,57 @@
+class IntroductoryView
+
+  def render
+puts <<-STRING
+Welcome to Ruby Flash Cards. To play, just enter the corrrreeect term for each 
+definition!!!!!!! If you wish to end the game, please type end at any time. 
+
+Ready? (We aren't asking, we're telling you that you are ...) 
+
+GOOO!!!
+STRING
+  end
+
+end
+
+class DefinitionView
+
+  def render(definition)
+    puts "Definition: \n #{definition}"
+  end
+
+end
+
+class TakeInputView
+
+  def render
+    print "Guess: "
+    gets.chomp
+  end
+
+end
+
+class FeedbackView
+
+  def render(correct_guess)
+    puts correct_guess ? "Correct" : "WRONGGGG!!!!!!!! TRY AGAIN!!!!!!!!"
+  end
+
+end
+
+IntroductoryView.new.render
+
+DefinitionView.new.render("Ducks are fast and fly!")
+
+puts "OK here comes our input taker ... "
+
+input = TakeInputView.new.render
+
+puts input
+
+correct = FeedbackView.new.render(true) #== "Correct"
+
+# puts correct == "Correct"
+
+incorrect = FeedbackView.new.render(false) #== "WRONGGGG!!!!!!!! TRY AGAIN!!!!!!!!"
+
+# puts incorrect == "WRONGGGG!!!!!!!! TRY AGAIN!!!!!!!!"
